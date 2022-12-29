60f548eb0882e23cd4f2dcae2b531222f4a615b8
Nathaniel Reinhart Iskandar <76016696+NathanielIskandar@users.noreply.github.com>
2022-12-28 19:09:10 -0800

    Add files via upload
    DoombaV3Final

---
+++ DoombaV3Final/src/main.cpp
@@ -0,0 +1,106 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Sirena K                                         */
+/*    Created:      Thu Dec 30 2021                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "robot-config.h"
+#include "driving-functions.h"
+#include "autonomous-functions.h"
+#include "routine.h"
+//-----NEW-----
+#include "customMenu.h"
+#include "GPS.h"
+#include "GPSRoutine.h"
+//-------------
+#include "routineRampSide.h"
+#include "autonSkills.h"
+
+using namespace vex;
+competition Competition;
+
+// // //Without Menu
+void userControl(){
+  while(1){
+    arcadeDrive();
+    //tankDrive();
+    liftControl();
+    liftToggle();
+    
+  }
+}
+void Autonomous(){
+  //safeGoalSide();
+  middlePriority();
+}
+
+//---Menu---
+// int tempStatus = 0;
+// void menuSetup(){
+//   menuCONFIG();
+//   notificationHUD("Robot: OK");
+// }
+// void Autonomous(){
+//     Brain.Screen.setCursor(1, 1);
+//     Brain.Screen.print("LiftL1 temp: %f\n", liftL1.temperature());
+//     Brain.Screen.print("LiftL2 temp: %f\n", liftL2.temperature());
+//     Brain.Screen.print("LiftR1 temp: %f\n", liftR1.temperature());
+//     Brain.Screen.print("LiftR2 temp: %f\n", liftR2.temperature());
+//   if(getValues(AUTON_TYPE) == GOALSIDE){
+//     safeGoalSide();
+//     printf("goal side auton");
+//   }else if(getValues(AUTON_TYPE) == RAMPSIDE){
+//     //rampSide();
+//     aimatCornerGPS();
+//     printf("ramp side auton");
+//   }else if(getValues(AUTON_TYPE) == SKILLS){
+//     score130points();
+//     printf("skills");
+//   }else{
+//     Brain.Screen.setCursor(10, 10);
+//     Brain.Screen.print("Auton not selected");
+//     notificationHUD("Auton not selected");
+//   }
+//   notificationHUD("Auton: Done");
+// }
+// void userControl(){
+//     if(getValues(AUTON_DRIVE) == ARCADE){
+//       arcadeDrive();
+//       liftToggle();
+//       liftControl();
+//     }else if(getValues(AUTON_DRIVE) == TANK){
+//       tankDrive();
+//       liftToggle();
+//       liftControl();
+//     }
+//   }
+
+// void pre_auton (void){
+//   menuCONFIG();
+//   notificationHUD("Robot: OK");
+// }
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  //pre_auton();
+
+  Competition.drivercontrol(userControl);
+  Competition.autonomous(Autonomous);
+
+  // while(1){
+  //   if(tempStatus != currStatus()){
+  //     statusHUD();
+  //     tempStatus = currStatus();
+  //   }
+  // }
+  
+}
